11b0d68ad963e3487a896a3cc01afb67a2ef6d4e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Collections;
+
+public partial class _Default : System.Web.UI.Page 
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ReportViewer1.Report = new XtraReport1();
+        
+        if (Session["skippedDetailBands"] != null)
+        {
+            Hashtable ht = (Hashtable)Session["skippedDetailBands"];
+
+            ((XtraReport1)ReportViewer1.Report).skippedDetailBands = ht;
+
+            if (ht.Contains(Request["grpid"]))
+                ht[Request["grpid"]] = !Convert.ToBoolean(ht[Request["grpid"]]);
+            else
+                ht.Add(Request["grpid"], true);
+        }
+        else
+            Session["skippedDetailBands"] = new Hashtable();
+    
+    }
+}
---
+++ VB/WebSite/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="vb" AutoEventWireup="true"  CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<%@ Register Assembly="DevExpress.XtraReports.v8.1.Web, Version=8.1.7.0, Culture=neutral, PublicKeyToken=9B171C9FD64DA1D1"
+	Namespace="DevExpress.XtraReports.Web" TagPrefix="dxxr" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+	<title>Untitled Page</title>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div>
+		&nbsp;<br />
+		<dxxr:ReportViewer ID="ReportViewer1" runat="server"
+			ReportName="XtraReport1" PageByPage="False">
+		</dxxr:ReportViewer>
+
+	</div>
+	</form>
+</body>
+</html>
